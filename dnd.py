edb6ea924ae12d88967a0bda88bf24345498e2b4
Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>
2025-05-31 00:43:09 +0200

    Update dnd.py
    Signed-off-by: Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>

--- dnd.py
+++ dnd.py
@@ -5,8 +5,14 @@ class Caracteristique:
     self.score = score
     self.bonus = (score//2)-5
     self.temp_mods = None
+    self.car_comps = []
 
-def update_score(self, mod)
+def update_score(self, mod=0)
   self.score += mod
   self.bonus = (self.score//2)-5
-  
+
+class Competences:
+  def __init__(self, name, carac):
+    self.name = name
+    carac.car_comps.append(self)
+    self.score = carac.bonus + 0 #maitrise

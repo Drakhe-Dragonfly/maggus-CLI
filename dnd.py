842acc787171329db75275807a451664274891d7
Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>
2025-05-30 23:59:57 +0200

    Create dnd.py
    Signed-off-by: Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>

---
+++ dnd.py
@@ -0,0 +1,12 @@
+class Caracteristique:
+  def __init__(self, name, abbreviation, score=10):
+    self.name = name
+    self.abbreviation = abbreviation
+    self.score = score
+    self.bonus = (score//2)-5
+    self.temp_mods = None
+
+def update_score(self, mod)
+  self.score += mod
+  self.bonus = (self.score//2)-5
+  

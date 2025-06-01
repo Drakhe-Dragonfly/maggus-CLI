0762169c39199796a66ce723fda5c7e68199b780
Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>
2025-06-01 02:38:47 +0200

    Create main.py
    Signed-off-by: Drakhe-Dragonfly <Drakhe_Dragonfly@protonmail.com>

---
+++ main.py
@@ -0,0 +1,23 @@
+import
+
+system_name_lists_dict = {"dnd 5e" : "DnD5e",
+                     "maggus" : "Maggus"}
+
+def main():
+  system_not_found = True
+  while system_not_found:
+    system = input("Quel système voulez-vous utiliser ?").lower() #prend le système que l'utilisateur veut utiliser
+     effective_system = system_name_lists[system]
+    if effective_system = "DnD5e": #vérifie
+      system_not_found = False
+      pass
+    elif effective_system = "Maggus":
+      system_not_found = False
+      pass
+    else:
+      print("Système non implémenté ou non reconnu. Voici la liste des systèmes :")
+      for key in system_name_lists_dict:
+        print(f"- {system_name_lists_dict[key]}")
+  print("Bonne journée.")
+
+main()
